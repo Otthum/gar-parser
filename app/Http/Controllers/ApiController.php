4b4b3dacb76d31d4ce425c4630eefd010e119b17
Aidar <aidar3012000@gmail.com>
2022-06-15 18:36:11 +0500

    Доделал поиск

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MunHierarchy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,20 +18,43 @@ class ApiController extends Controller
         }
 
         $query = <<<EOT
-            select mh.gar_id, address_str, h.num, match (address_str) against (?) as rel
+            select
+                mh.gar_id,
+                match (address_str) against (? in boolean mode) as rel
             from mun_hierarchies mh
-            left join houses h on h.gar_id = mh.gar_id
             where
-                match (address_str) against (?) > 0 and
+                match (address_str) against (?  in boolean mode) > 0 and
                 mh.is_active = 1
+            limit 200
         EOT;
 
         $search = preg_replace('~, *~', ' ', $search);
-        $items = explode(' ', $search);
 
-        $results = DB::select($query, [$search, $search]);
+        $ids = DB::select($query, [$search, $search]);
+
+        $idsToFetch = [];
+        $biggestRel = $ids[0]->rel;
+        foreach ($ids as $item) {
+            if ($item->rel == $biggestRel) {
+                $idsToFetch[] = $item->gar_id;
+            } else {
+                break;
+            }
+        }
+
+        $results = MunHierarchy::select(
+            'mun_hierarchies.gar_id',
+            'address_str',
+        )
+            ->leftJoin('houses', 'houses.gar_id', 'mun_hierarchies.gar_id')
+            ->leftJoin('addr_objs', 'addr_objs.gar_id', 'mun_hierarchies.gar_id')
+            ->whereIn('mun_hierarchies.gar_id', $idsToFetch)
+            ->orderByRaw(
+                'cast(houses.num as signed) asc, length(houses.num) asc, cast(houses.num_1 as signed) asc'
+            )
+            ->limit($req->limit ?? 10)
+            ->get()->toArray();        
         
-
-        dump($results);
+        return response()->json($results);
     }
 }
