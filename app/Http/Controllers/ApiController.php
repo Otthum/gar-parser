bb4c4dd7124bea1886c90f5c0dbee76f22f33f03
Aidar <aidar3012000@gmail.com>
2023-02-22 16:24:18 +0500

    Убрал использование конвертера, т.к. теперь адреса используют полные наименования.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AddrObj;
+use App\Models\Apartment;
+use App\Models\House;
 use App\Models\MunHierarchy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -9,6 +12,13 @@ use App\Services\SearchTermsConverterService as Converter;
 
 class ApiController extends Controller
 {
+    protected array $objectsToInclude = [
+        Apartment::class => true,
+        House::class => true,
+        AddrObj::class => true
+    ];
+
+
     public function search(Request $req)
     {
         if ( !($search = $req->q) ) {
@@ -20,8 +30,7 @@ class ApiController extends Controller
 
         $query = <<<EOT
             select
-                mh.gar_id,
-                match (address_str) against (? in boolean mode) as rel
+                gar_id                
             from mun_hierarchies mh
             where
                 match (address_str) against (?  in boolean mode) > 0 and
@@ -30,9 +39,28 @@ class ApiController extends Controller
         EOT;
 
         $search = preg_replace('~, *~', ' ', $search);
-        $search = Converter::convert($search);
+        $terms = explode(' ', $search);
 
-        $ids = DB::select($query, [$search, $search]);
+        $formattedSearch = '';
+        foreach ($terms as $key => $term) {
+
+            if (trim($term) == '') {
+                continue;
+            }
+
+            $formattedSearch .= '+' . $term;
+            if (array_key_last($terms) != $key) {
+                $formattedSearch .= " ";
+            }
+            
+        }
+
+        $formattedSearch .= '*';
+
+        $ids = DB::select($query, [$formattedSearch]);
+        $ids = array_map(function ($value) {
+            return $value->gar_id;
+        }, $ids);
 
         if (count($ids) > 0) {
             $addresses = $this->getAddressesFromIds($ids);
@@ -41,7 +69,7 @@ class ApiController extends Controller
         $results = [
             'items' => $addresses ?? [],
             'result' => true,
-            'search' => $search,
+            'search' => $formattedSearch,
         ];
         
         return response()->json($results);
@@ -49,27 +77,31 @@ class ApiController extends Controller
 
     protected function getAddressesFromIds(array $ids, int $limit = 10)
     {
-        $idsToFetch = [];
-        $biggestRel = $ids[0]->rel;
-        foreach ($ids as $item) {
-            if ($item->rel == $biggestRel) {
-                $idsToFetch[] = $item->gar_id;
-            } else {
-                break;
-            }
-        }
-
         $addresses = MunHierarchy::select(
             'mun_hierarchies.gar_id',
             'address_str',
         )
             ->leftJoin('houses', 'houses.gar_id', 'mun_hierarchies.gar_id')
             ->leftJoin('addr_objs', 'addr_objs.gar_id', 'mun_hierarchies.gar_id')
-            ->whereIn('mun_hierarchies.gar_id', $idsToFetch)
+            ->leftJoin('apartments', 'apartments.gar_id', 'mun_hierarchies.gar_id')
+            ->whereIn('mun_hierarchies.gar_id', $ids)
             ->orderByRaw(
-                'cast(houses.num as signed) asc, length(houses.num) asc, cast(houses.num_1 as signed) asc'
+                'length(address_str) asc, address_str asc'
             )
             ->limit($limit)
+            ->where(function ($q) {
+                if ($this->objectsToInclude[Apartment::class])  {
+                    $q->orWhereNotNull('apartments.gar_id');
+                }
+        
+                if ($this->objectsToInclude[House::class]) {
+                    $q->orWhereNotNull('houses.gar_id');
+                }
+        
+                if ($this->objectsToInclude[AddrObj::class]) {
+                    $q->orWhereNotNull('addr_objs.gar_id');
+                }
+            })
             ->get()
             ->toArray();
 
